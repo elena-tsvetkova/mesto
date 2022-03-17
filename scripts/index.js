801610864121e5eb5339a4c5b837c7ad5b312919
Елена <lena.lena1803@yandex.ru>
2022-03-18 00:23:56 +0300

    js

--- scripts/index.js
+++ scripts/index.js
@@ -3,19 +3,22 @@ let elementClose = document.querySelector('.popup__close');
 let name = document.querySelector('.profile__name');
 let status = document.querySelector('.profile__status');
 let formElement = document.querySelector('.popup__form');
+const popup = document.querySelector('.popup')
 
-function showClick() {
-  document.querySelector('.popup_opened').style.display = 'flex';
+function popupOpen() {
+ popup.classList.add('popup_opened')
+ document.querySelector('.popup__input_name').value = name.textContent
+ document.querySelector('.popup__input_status').value = status.textContent
 }
 
-element.addEventListener('click', showClick); 
+element.addEventListener('click', popupOpen); 
 
 
 function close() {
-  document.querySelector('.popup_opened').style.display = 'none';
+  popup.classList.remove('popup_opened')
 }
 
-elementClose.addEventListener('click', close); 
+elementClose.addEventListener('click', close)
 
 
 
