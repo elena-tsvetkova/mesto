4e81cbcd1230dee68276aa88846e342564fba6c4
Елена <lena.lena1803@yandex.ru>
2022-03-22 23:33:02 +0300

    popup для +

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,16 @@
 let element = document.querySelector('.profile__button-edit');
-let elementClose = document.querySelector('.popup__close');
+let elementClose = document.querySelector('.popup__close-profil');
 let name = document.querySelector('.profile__name');
 let status = document.querySelector('.profile__status');
-let formElement = document.querySelector('.popup__form');
-const popup = document.querySelector('.popup')
+let formElement = document.querySelector('.popup__form-profile');
+const popup = document.querySelector('.popup__profile');
 let namePopup = document.querySelector('.popup__input_type_name');
 let jobPopup = document.querySelector('.popup__input_type_job');
+const newImage = document.querySelector('.popup__new-image');
+const nameImage = document.querySelector('.popup__input_type_title');
+const linkImage = document.querySelector('.popup__input_type_link');
+const addImage = document.querySelector('.profile__add-button');
+const closeAddButton = document.querySelector('.popup__close-add');
 
 function popupOpen() {
  popup.classList.add('popup_opened')
@@ -22,9 +27,6 @@ function close() {
 
 elementClose.addEventListener('click', close)
 
-
-
-
 function formSubmitHandler(evt) {
   evt.preventDefault();
   let nameInput = namePopup.value;
@@ -36,3 +38,17 @@ function formSubmitHandler(evt) {
 }
 
 formElement.addEventListener('submit', formSubmitHandler); 
+
+function popupNewImage() {
+  newImage.classList.add('popup_opened')
+  nameImage.value = title.textContent
+  linkImage.value = link.textContent
+ }
+ 
+ addImage.addEventListener('click', popupNewImage); 
+ 
+ function closeAdd() {
+  newImage.classList.remove('popup_opened')
+}
+
+closeAddButton.addEventListener('click', closeAdd)
