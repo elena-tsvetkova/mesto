b8a93ed4c5b6bd065d53c416a234c4b4c7dcd4b9
Елена <lena.lena1803@yandex.ru>
2022-03-23 23:38:12 +0300

    добавление карточки

--- scripts/index.js
+++ scripts/index.js
@@ -6,11 +6,18 @@ let formElement = document.querySelector('.popup__form-profile');
 const popup = document.querySelector('.popup__profile');
 let namePopup = document.querySelector('.popup__input_type_name');
 let jobPopup = document.querySelector('.popup__input_type_job');
+
 const newImage = document.querySelector('.popup__new-image');
 const nameImage = document.querySelector('.popup__input_type_title');
 const linkImage = document.querySelector('.popup__input_type_link');
 const addImage = document.querySelector('.profile__add-button');
 const closeAddButton = document.querySelector('.popup__close-add');
+
+let cardNow = document.querySelector('.popup__input_type_title');
+let cardSrc = document.querySelector('.popup__input_type_link');
+
+let link = document.querySelector('.element__image');
+let title = document.querySelector('.element__title');
 
 function popupOpen() {
  popup.classList.add('popup_opened')
@@ -41,8 +48,7 @@ formElement.addEventListener('submit', formSubmitHandler);
 
 function popupNewImage() {
   newImage.classList.add('popup_opened')
-  nameImage.value = title.textContent
-  linkImage.value = link.textContent
+  
  }
  
  addImage.addEventListener('click', popupNewImage); 
@@ -52,3 +58,59 @@ function popupNewImage() {
 }
 
 closeAddButton.addEventListener('click', closeAdd)
+
+const  card= document.querySelector('.elements');
+
+let cardTemplate = document.querySelector('.element-template').content;
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+initialCards.forEach(function (element) {
+  const cardElement = cardTemplate.cloneNode(true);
+
+  cardElement.querySelector('.element__image').src = element.link;
+  cardElement.querySelector('.element__title').textContent = element.name;
+  
+
+  card.append(cardElement)
+})
+
+function formSubmitImage(evt) {
+  evt.preventDefault();
+  // let nameCard = cardNow.value;
+  // let cardLink = cardSrc.value;
+
+  
+  // initialCards.unshift({'nameCard', 'cardLink'})
+  
+  // name.textContent = nameInput;
+  // status.textContent = jobInput;
+  close();
+}
+
+formElement.addEventListener('submit', formSubmitImage); 
